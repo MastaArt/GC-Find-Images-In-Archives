729564c44085c3243ba3df689b04c7d04e8e28af
MAXSCRIPT <lukuanenko@gmail.com>
2024-10-10 21:06:46 +0300

    1.0.0

--- extract.bat
+++ extract.bat
@@ -10,11 +10,13 @@ cls
 set input=%1
 set arg1=
 set arg2=
+set fmt=
 set ext=
 
-for /f "tokens=1,2 delims=;" %%I in (%input%) do (
+for /f "tokens=1,2,3 delims=;" %%I in (%input%) do (
 	set arg1=%%I
 	set arg2=%%J
+	set fmt=%%K
 )
 
 for %%f in ("%arg1%") do (
@@ -24,15 +26,15 @@ for %%f in ("%arg1%") do (
 mkdir "%arg2%"
 echo %ext%
 if /i %ext%==.zip (
-	7za.exe x "%arg1%" -o"%arg2%" -y -r *.jpg *.png *.jpeg -xr!resources -xr!PIX -xr!render -xr!renders -xr!rendersoutput -xr!renderoutput
+	7za.exe x "%arg1%" -o"%arg2%" -y -r %fmt% -xr!resources -xr!PIX -xr!render -xr!renders -xr!rendersoutput -xr!renderoutput
 )
  
 if /i %ext%==.7z (
-	7za.exe x "%arg1%" -o"%arg2%" -y -r *.jpg *.png *.jpeg -xr!resources -xr!PIX -xr!render -xr!renders -xr!rendersoutput -xr!renderoutput
+	7za.exe x "%arg1%" -o"%arg2%" -y -r %fmt% -xr!resources -xr!PIX -xr!render -xr!renders -xr!rendersoutput -xr!renderoutput
 )
 
 if /i %ext%==.rar (
-	"C:\Program Files\WinRAR\WinRAR.exe" x -y -ibck -x*resources* -x*PIX* -x*render* -x*renders* -x*rendersoutput* -x*renderoutput* "%arg1%" "%arg2%" *.jpg *.jpeg *.png
+	"C:\Program Files\WinRAR\WinRAR.exe" x -y -ibck -x*resources* -x*PIX* -x*render* -x*renders* -x*rendersoutput* -x*renderoutput* "%arg1%" "%arg2%" %fmt%
 )
 
 
